014914f613107a3f438cc317f65c9f11c3ca0ad2
Alexandre Cesar <acmorcego@gmail.com>
2019-02-25 11:34:12 -0300

    parse number and range values

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import { useState, useEffect } from 'react'
 
 const noValidation = () => ({})
 
+function parseValue(type, value, checked) {
+  if (type === 'checkbox') return checked
+  if (type === 'number' || type === 'range') {
+    const parsed = parseFloat(value)
+    if (isNaN(parsed)) return undefined
+    return parsed
+  }
+  return value
+}
+
 function useForm({ initialValues, validate = noValidation, onSubmit }) {
   const [values, setValues] = useState(initialValues)
   const [errors, setErrors] = useState({})
@@ -17,7 +27,7 @@ function useForm({ initialValues, validate = noValidation, onSubmit }) {
   const handleChange = ({ target: { name, type, value, checked } }) =>
     setValues({
       ...values,
-      [name]: type === 'checkbox' ? checked : value
+      [name]: parseValue(type, value, checked)
     })
 
   const handleSubmit = event => {
